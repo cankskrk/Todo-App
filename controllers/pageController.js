9da53a3adb347736cc978b8d606ae6a555659db9
cankskrk <ardacankisakurek@gmail.com>
2022-11-27 04:39:49 +0100

    Update Posting Task Operation

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,6 +1,9 @@
 const Task = require('../models/TodoList');
 const CompletedTask = require('../models/CompletedList');
 
-exports.getPage = (req, res) => {
-  res.render('main');
+exports.getPage = async (req, res) => {
+  const task = await Task.find({});
+  res.render('main', {
+    task,
+  });
 };
